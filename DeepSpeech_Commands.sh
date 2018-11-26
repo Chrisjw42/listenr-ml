15b6b6ff09ca019bd4a114df87412a157d1b1378
Chris Swan <swachris@gmail.com>
2018-11-26 11:12:38 +0800

    Update DeepSpeech_Commands.sh
    fixed few bugs so it runs all the way through - still returning 'Op' error at the end however

--- DeepSpeech_Commands.sh
+++ DeepSpeech_Commands.sh
@@ -11,8 +11,9 @@ wget https://github.com/mozilla/DeepSpeech/releases/download/v0.1.0/audio-0.1.0.
 tar -xvzf audio-0.1.0.tar.gz
 wget https://github.com/mozilla/DeepSpeech/releases/download/v0.1.0/deepspeech-0.1.0-models.tar.gz
 tar -xvzf deepspeech-0.1.0-models.tar.gz
-pip3 install deepspeech
+pip install deepspeech
+chmod u+x DeepSpeech/util/taskcluster.py
 DeepSpeech/util/taskcluster.py --target models/ --artifact convert_graphdef_memmapped_format --source tensorflow
-models/convert_graphdef_memmapped_format --in_graph=output_graph.pb --out_graph=output_graph.pbmm
-./deepspeech --model models/output_graph.pbmm --alphabet models/alphabet.txt --lm models/lm.binary --trie models/trie --audio audio/2830-3980-0043.wav
-
+chmod u+x models/convert_graphdef_memmapped_format
+models/convert_graphdef_memmapped_format --in_graph=models/output_graph.pb --out_graph=models/output_graph.pbmm
+deepspeech --model models/output_graph.pbmm --alphabet models/alphabet.txt --lm models/lm.binary --trie models/trie --audio audio/2830-3980-0043.wav
