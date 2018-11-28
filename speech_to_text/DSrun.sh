60255557a6460070a5dddb80a712f9082b37293e
Chris Swan <swachris@gmail.com>
2018-11-28 13:10:25 +0800

    Packages added - safety push
    Still need to do some thorough testing - hoping have resolved previous model fail

--- speech_to_text/DSrun.sh
+++ speech_to_text/DSrun.sh
@@ -1,19 +1,22 @@
-mkdir DeepSpeech
-cd DeepSpeech
-sudo apt-get update
+#!/bin/bash
+sudo apt-get -y update
 curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh
-sudo apt-get install git-lfs
-sudo apt-get install python3.6
-sudo apt-get install python3-pip
+sudo apt-get -y install git-lfs
+sudo apt-get -y install python3.6
+sudo apt-get -y install python3-pip
 git clone https://github.com/mozilla/DeepSpeech
-python3 DeepSpeech/util/taskcluster.py --target
+cd DeepSpeech
+python3 util/taskcluster.py --target
 wget https://github.com/mozilla/DeepSpeech/releases/download/v0.1.0/audio-0.1.0.tar.gz
 tar -xvzf audio-0.1.0.tar.gz
 wget https://github.com/mozilla/DeepSpeech/releases/download/v0.1.0/deepspeech-0.1.0-models.tar.gz
 tar -xvzf deepspeech-0.1.0-models.tar.gz
 pip3 install deepspeech
-chmod u+x DeepSpeech/util/taskcluster.py
-DeepSpeech/util/taskcluster.py --target models/ --artifact convert_graphdef_memmapped_format --source tensorflow
+pip3 install -r requirements.txt
+chmod u+x util/taskcluster.py
+util/taskcluster.py --target models/ --artifact convert_graphdef_memmapped_format --source tensorflow
 chmod u+x models/convert_graphdef_memmapped_format
 models/convert_graphdef_memmapped_format --in_graph=models/output_graph.pb --out_graph=models/output_graph.pbmm
 deepspeech --model models/output_graph.pbmm --alphabet models/alphabet.txt --lm models/lm.binary --trie models/trie --audio audio/2830-3980-0043.wav
+# currently only WAVE files with 16-bit, 16 kHz, mono are supported in the Python client
+
