ff952a9160674a2eaadd359db2e740c6347e303f
Chris Swan <swachris@gmail.com>
2018-11-29 12:16:50 +0800

    Model and audio version updated
    According to DeepSpeech developers, this was the cause of error.  Currently running testing

--- speech_to_text/DSrun.sh
+++ speech_to_text/DSrun.sh
@@ -7,10 +7,10 @@ sudo apt-get -y install python3-pip
 git clone https://github.com/mozilla/DeepSpeech
 cd DeepSpeech
 python3 util/taskcluster.py --target
-wget https://github.com/mozilla/DeepSpeech/releases/download/v0.1.0/audio-0.1.0.tar.gz
-tar -xvzf audio-0.1.0.tar.gz
-wget https://github.com/mozilla/DeepSpeech/releases/download/v0.1.0/deepspeech-0.1.0-models.tar.gz
-tar -xvzf deepspeech-0.1.0-models.tar.gz
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.3.0/audio-0.3.0.tar.gz
+tar -xvzf audio-0.3.0.tar.gz
+wget https://github.com/mozilla/DeepSpeech/releases/download/v0.3.0/deepspeech-0.3.0-models.tar.gz
+tar -xvzf deepspeech-0.3.0-models.tar.gz
 pip3 install deepspeech
 pip3 install -r requirements.txt
 chmod u+x util/taskcluster.py
@@ -19,4 +19,3 @@ chmod u+x models/convert_graphdef_memmapped_format
 models/convert_graphdef_memmapped_format --in_graph=models/output_graph.pb --out_graph=models/output_graph.pbmm
 deepspeech --model models/output_graph.pbmm --alphabet models/alphabet.txt --lm models/lm.binary --trie models/trie --audio audio/2830-3980-0043.wav
 # currently only WAVE files with 16-bit, 16 kHz, mono are supported in the Python client
-
